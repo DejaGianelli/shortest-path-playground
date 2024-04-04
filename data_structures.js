95531c52070f4444010a419e86d9966a00580bf1
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2024-04-04 20:58:37 -0300

    Add object that represents a PriorityQueue data structure

---
+++ data_structures.js
@@ -0,0 +1,48 @@
+/**
+ * Represents a Priority Queue
+ * @constructor
+ * @param {Comparator} cmp - Object that implements Comparator interface
+ */
+function PriorityQueue(cmp) {
+    /**
+     * Array of Object
+     * @type {Object[]}
+     */
+    let items = []
+    let count = 0
+    /**
+     * Function that compare two objects
+     * @type {Function}
+     */
+    let comparator = cmp;
+
+    /**
+     * Adds an item in the queue
+     * @param {Object} item - Some item
+     */
+    this.add = function(item) {
+        let i = 0
+        for (i = (count - 1); i >= 0; i--) {
+            if (comparator(items[i], item) > 0) {
+                items[i + 1] = items[i]
+            } else {
+                break
+            }
+        }
+        i = i + 1
+        items[i] = item
+        count++
+    }
+
+    /**
+     * Removes a item from the queue by priority
+     * @returns {Object} item - Removed item
+     */
+    this.remove = function() {
+        if (count == 0)
+            throw Error("Queue is empty!")
+        return items[--count]
+    }
+}
+
+export { PriorityQueue }
---
+++ tests/PriorityQueue.test.js
@@ -0,0 +1,27 @@
+import { describe, expect, test } from '@jest/globals';
+import { PriorityQueue } from '../data_structures';
+
+describe('Priority Queue', () => {
+    test('Adds items and remove them by priority', () => {
+
+        const comparator = (obj, to) => {
+            if (obj > to) {
+                return 1
+            } else if (obj === to) {
+                return 0
+            } else {
+                return -1
+            }
+        };
+
+        var queue = new PriorityQueue(comparator);
+
+        queue.add(4)
+        queue.add(5)
+        queue.add(2)
+
+        expect(queue.remove()).toBe(5);
+        expect(queue.remove()).toBe(4);
+        expect(queue.remove()).toBe(2);
+    });
+});
