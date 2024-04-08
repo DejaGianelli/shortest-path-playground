7ed621f183b1ba0a1f6bac41a1399370cbb539a6
Dejamir Gianelli <dejamir.silva@sensedia.com>
2024-04-07 21:51:31 -0300

    Adds random colors to cities
    Show city's ids in the canvas

--- app/models/City.js
+++ app/models/City.js
@@ -10,15 +10,29 @@ export default function City(center, size) {
     const _id = uuidv4()
     const _center = center
     const _size = size
+    const color = generateColor()
 
     function radius() {
         return size / 2
+    }
+
+    function generateColor() {
+        const randomNum = Math.floor(Math.random() * 16777215)
+        let hexNum = randomNum.toString(16);
+        hexNum = hexNum.padStart(6, '0');
+        return "#" + hexNum;
+    }
+
+    function getIdShort() {
+        return _id.slice(0, 4)
     }
 
     return {
         id: _id,
         center: _center,
         size: _size,
-        radius
+        radius,
+        color,
+        getIdShort
     }
 }
--- app/view/render.js
+++ app/view/render.js
@@ -8,20 +8,26 @@ export default function renderCanvas(cities, paths, size, canvas) {
     context.clearRect(0, 0, size.width, size.height)
 
     for (const [, city] of Object.entries(cities)) {
-        context.beginPath();
-        context.arc(city.center.x, city.center.y, city.radius(), 0, 2 * Math.PI);
-        context.stroke();
+        context.fillStyle = city.color
+        context.beginPath()
+        context.arc(city.center.x, city.center.y, city.radius(), 0, 2 * Math.PI)
+        context.fill()
+
+        context.textAlign = "center"
+        context.textBaseline = "middle"
+        context.fillStyle = "#000000"
+        context.fillText(city.getIdShort(), city.center.x, city.center.y)
     }
     
     for (const [, path] of Object.entries(paths)) {
         for (const segment of path.segments) {
-            context.beginPath();
-            context.strokeStyle = "black";
-            context.lineWidth = 1;
-            context.moveTo(segment.p1.x, segment.p1.y);
-            context.lineTo(segment.p2.x, segment.p2.y);
-            context.stroke();
-            context.closePath();
+            context.beginPath()
+            context.strokeStyle = "black"
+            context.lineWidth = 1
+            context.moveTo(segment.p1.x, segment.p1.y)
+            context.lineTo(segment.p2.x, segment.p2.y)
+            context.stroke()
+            context.closePath()
         }
     }
     
